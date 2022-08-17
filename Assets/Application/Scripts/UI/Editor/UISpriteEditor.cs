f059fa06ec1a31382bc637675d443925b84d58ae
Yusuke Kurokawa <wotakuro@gmail.com>
2022-08-18 00:50:35 +0900

    update

--- Assets/Application/Scripts/UI/Editor/UISpriteEditor.cs
+++ Assets/Application/Scripts/UI/Editor/UISpriteEditor.cs
@@ -1,13 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 
 namespace BMProject.UI
 {
-    public class UISpriteEditor : MonoBehaviour
+    [CustomEditor(typeof(UISprite))]
+    public class UISpriteEditor : Editor
     {
+        private bool debugRendereBounds = false;
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+            var obj = target as UISprite;
+            if(GUILayout.Button("Calc Box"))
+            {
+                obj.CalculateSelfBox();
+            }
 
+            debugRendereBounds = EditorGUILayout.Toggle("DebugGizmo",debugRendereBounds);
+            obj.debugDrawGizmo = debugRendereBounds;
+
+        }
 
     }
 }
--- Assets/Application/Scripts/UI/UISprite.cs
+++ Assets/Application/Scripts/UI/UISprite.cs
@@ -21,15 +21,20 @@ namespace BMProject.UI
         [SerializeField]
         private Vector2 m_position;
         [SerializeField]
-        private HorizontalPoint horizontalPoint;
+        private HorizontalPoint horizontalPoint = HorizontalPoint.Center;
         [SerializeField]
-        private VerticalPoint verticalPoint;
+        private VerticalPoint verticalPoint = VerticalPoint.Center;
 
         [SerializeField]
         private int depth;
 
         [SerializeField]
         private float rotateAngle;
+
+        [SerializeField]
+        private Vector2 selfBoxCenter;
+        [SerializeField]
+        private Vector2 selfBoxSize;
 
 
         // Update is called once per frame
@@ -43,39 +48,47 @@ namespace BMProject.UI
 
             Vector3 position;
             position.z = 1.0f + depth * 0.1f;
-            position.x = CulcPositionX();
-            position.y = CulcPositionY();
+            position.x = GetActualPositionX();
+            position.y = GetActualPositionY();
 
             // X position
             transform.localPosition = position;
-            transform.localRotation = CulcRotation();
+            transform.localRotation = GetActualRotation();
         }
 
-        private Quaternion CulcRotation(){
+        private Quaternion GetActualRotation(bool forceNonRotate = false)
+        {
             var uiCamera = UICamera.Instance;
             float offset = 0.0f;
-            switch(uiCamera.Rotate){
-                case UICamera.RotateType.RightUp:
-                    offset = -90;
-                    break;
-                case UICamera.RotateType.LeftUp:
-                    offset = 90;
-                    break;
+            if (!forceNonRotate)
+            {
+                switch (uiCamera.Rotate)
+                {
+                    case UICamera.RotateType.RightUp:
+                        offset = -90;
+                        break;
+                    case UICamera.RotateType.LeftUp:
+                        offset = 90;
+                        break;
+                }
             }
             return Quaternion.Euler(0,0 ,rotateAngle + offset);
         }
 
-        private float CulcPositionX(){
+        private float GetActualPositionX(bool forceNonRotate = false){
             var uiCamera = UICamera.Instance;
             Vector2 virtualScreenSize = uiCamera.virtualScreenSize;
-            if(uiCamera.Rotate ==  UICamera.RotateType.None){
+            if(uiCamera.Rotate ==  UICamera.RotateType.None || forceNonRotate)
+            {
                 switch(horizontalPoint){
                     case HorizontalPoint.Left:
-                        return m_position.x * virtualScreenSize.x  - virtualScreenSize.x;         
+                        return m_position.x * virtualScreenSize.x  - virtualScreenSize.x
+                            + selfBoxSize.x * 0.5f - selfBoxCenter.x;         
                     case HorizontalPoint.Center:
-                        return m_position.x * virtualScreenSize.x;
+                        return m_position.x * virtualScreenSize.x - selfBoxCenter.x;
                     case HorizontalPoint.Right:
-                        return m_position.x  * virtualScreenSize.x + virtualScreenSize.x;
+                        return m_position.x  * virtualScreenSize.x + virtualScreenSize.x
+                            - selfBoxSize.x * 0.5f - selfBoxCenter.x;
                 }
             }
             else{
@@ -85,28 +98,32 @@ namespace BMProject.UI
                 }
                 switch(verticalPoint){
                     case VerticalPoint.Up:
-                        return (m_position.y * virtualScreenSize.x  + virtualScreenSize.x ) * offscale;         
+                        return (m_position.y * virtualScreenSize.x  + virtualScreenSize.x
+                            - selfBoxSize.y * 0.5f) * offscale - selfBoxCenter.y;         
                     case VerticalPoint.Center:
-                        return (m_position.y * virtualScreenSize.x ) * offscale;
+                        return (m_position.y * virtualScreenSize.x ) * offscale - selfBoxCenter.y;
                     case VerticalPoint.Down:
-                        return (m_position.y  * virtualScreenSize.x - virtualScreenSize.x) * offscale;
+                        return (m_position.y  * virtualScreenSize.x - virtualScreenSize.x
+                            + selfBoxSize.y * 0.5f) * offscale - selfBoxCenter.y;
                 }
             }
             
             return 0.0f;
         }
 
-        private float CulcPositionY(){
+        private float GetActualPositionY(bool forceNonRotate = false){
             var uiCamera = UICamera.Instance;
             Vector2 virtualScreenSize = uiCamera.virtualScreenSize;
-            if(uiCamera.Rotate ==  UICamera.RotateType.None){
+            if(uiCamera.Rotate ==  UICamera.RotateType.None || forceNonRotate){
                 switch(verticalPoint){
                     case VerticalPoint.Up:
-                        return m_position.y * virtualScreenSize.y  + virtualScreenSize.y;         
+                        return m_position.y * virtualScreenSize.y  + virtualScreenSize.y
+                            - selfBoxSize.y * 0.5f - selfBoxCenter.y;
                     case VerticalPoint.Center:
-                        return m_position.y * virtualScreenSize.y;
+                        return m_position.y * virtualScreenSize.y - selfBoxCenter.y;
                     case VerticalPoint.Down:
-                        return m_position.y  * virtualScreenSize.y - virtualScreenSize.y;
+                        return m_position.y  * virtualScreenSize.y - virtualScreenSize.y
+                            + selfBoxSize.y * 0.5f - selfBoxCenter.y;
                 }
             }
             else{
@@ -116,16 +133,82 @@ namespace BMProject.UI
                 }
                 switch(horizontalPoint){
                     case HorizontalPoint.Left:
-                        return (m_position.x * virtualScreenSize.x  + virtualScreenSize.y ) * offscale;         
+                        return (m_position.x * virtualScreenSize.x  + virtualScreenSize.y
+                            - selfBoxSize.x * 0.5f) * offscale - selfBoxCenter.x;         
                     case HorizontalPoint.Center:
-                        return (m_position.x * virtualScreenSize.y ) * offscale;
+                        return (m_position.x * virtualScreenSize.y ) * offscale - selfBoxCenter.x;
                     case HorizontalPoint.Right:
-                        return (m_position.x  * virtualScreenSize.x - virtualScreenSize.y ) * offscale;
+                        return (m_position.x  * virtualScreenSize.x - virtualScreenSize.y
+                            + selfBoxSize.x * 0.5f) * offscale - selfBoxCenter.x;
                 }
             }
 
             return 0.0f;
         }
-        
+
+        public void CalculateSelfBox()
+        {
+            // set non rotate
+            Vector3 position;
+            position.z = 1.0f + depth * 0.1f;
+            position.x = GetActualPositionX(true);
+            position.y = GetActualPositionY(true);
+            transform.localPosition = position;
+            transform.localRotation = GetActualRotation(true);
+
+            //
+            var bounds = GetSelfBoundingBox();
+            this.selfBoxCenter.x = (bounds.center.x - transform.position.x );
+            this.selfBoxCenter.y = (bounds.center.y- transform.position.y );
+
+            this.selfBoxSize.x = bounds.size.x;
+            this.selfBoxSize.y = bounds.size.y;
+
+            // with rotate
+            position.x = GetActualPositionX();
+            position.y = GetActualPositionY();
+            transform.localPosition = position;
+            transform.localRotation = GetActualRotation();
+
+        }
+
+        private Bounds GetSelfBoundingBox()
+        {
+            var renderers = this.GetComponentsInChildren<Renderer>(true);
+
+            Bounds bounds = new Bounds();
+            bool isFirst = true;
+            foreach (var renderer in renderers)
+            {
+                if (isFirst)
+                {
+                    bounds = renderer.bounds;
+                    isFirst = false;
+                }
+                else
+                {
+                    bounds.Encapsulate(renderer.bounds);
+                }
+            }
+            return bounds;
+        }
+
+#if UNITY_EDITOR 
+        public bool debugDrawGizmo { get; set; }
+        private void OnDrawGizmosSelected()
+        {
+            if (debugDrawGizmo)
+            {
+                var backupColor = Gizmos.color;
+                Gizmos.color = Color.red;
+                Bounds bounds = GetSelfBoundingBox();
+                Gizmos.DrawCube(bounds.center, bounds.size);
+                Gizmos.color = backupColor;
+            }
+
+        }
+
+#endif
+
     }
 }
